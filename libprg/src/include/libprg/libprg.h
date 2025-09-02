d6c86c410a2feb1e6347cdc262f937ce773cde33
luise <luis.e2003@aluno.ifsc.edu.br>
2025-09-02 10:01:49 -0300

    Importada a imlementação feita em lab_2.c

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,6 +1,8 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
 
+typedef struct pilha pilha_t;
 
+pilha_t *criar_pilha(int capacidade);
 
 #endif
---
+++ libprg/src/libprg/pilha.c
@@ -0,0 +1,36 @@
+//
+// Created by luise on 02/09/2025.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct pilha {
+    int *elementos,
+        topo,
+        capacidade;
+} pilha_t;
+
+pilha_t *criar_pilha(int capacidade) {
+    pilha_t *p = malloc(sizeof(pilha_t));
+    p->elementos = malloc(capacidade*sizeof(int));
+    p->topo = -1;
+    p->capacidade = capacidade;
+    return p;
+}
+/*
+void empilhar() {
+
+}
+
+void desempilhar() {
+
+}
+
+void tamanho() {
+
+}
+
+void vazia() {
+
+}
+*/
