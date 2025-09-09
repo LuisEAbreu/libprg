d8692dda20bf721d2600033c2fb826311b5e72ed
Luis E <luis.e2003@aluno.ifsc.edu.br>
2025-09-09 11:17:35 -0300

    Ajustes visuais

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,6 +1,7 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
 
+/*--PILHA--*/
 typedef struct pilha pilha_t;
 
 pilha_t *criar_pilha(int capacidade);
@@ -10,5 +11,8 @@ int topo(pilha_t *p);
 int tamanho(pilha_t *p);
 int vazia(pilha_t *p);
 int destruir_pilha(pilha_t *p);
+
+/*--FILA--*/
+
 
 #endif
