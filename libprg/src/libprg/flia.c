2b2e21df88ab7e9e35f60e9459158d6df950713d
Luis E <luis.e2003@aluno.ifsc.edu.br>
2025-09-09 10:35:55 -0300

    Implementa a fila como struct

--- libprg/src/libprg/flia.c
+++ libprg/src/libprg/flia.c
@@ -1,3 +1,13 @@
+#include "libprg/libprg.h"
+
+typedef struct fila {
+    int *elementos,
+        tamanho,
+        inicio,
+        fim,
+        capacidade;
+}fila_t;
+
 // criar_fila
 // enfileirar
 // desenfileirar
