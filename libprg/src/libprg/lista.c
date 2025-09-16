966ff858f05c65dbcc1a70d477bedef7621e6bab
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2025-09-16 10:37:23 -0300

    Implementa função de criar lista linear.

--- libprg/src/libprg/lista.c
+++ libprg/src/libprg/lista.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "libprg/libprg.h"
 
 typedef struct lista_linear {
@@ -5,3 +6,17 @@ typedef struct lista_linear {
         tamanho,
         capacidade;
 }lista_linear_t;
+
+lista_linear_t *criar_lista_linear(int capacidade) {
+    lista_linear_t *lista = malloc(sizeof(lista_linear_t));
+    lista->elementos = malloc(sizeof(int)*capacidade);
+    lista->tamanho = 0;
+
+    return lista;
+}
+// inserir
+// buscar
+// remover
+// vazia
+// cheia
+// destruir
