9ce6c753bae9ef8e5a2be5853bb22a16e3d75977
luis <luiseduardodeabreu@gmail.com>
2025-09-17 20:35:38 -0300

    Inicializa parâmetro capacidade, esquecido na implementação

--- libprg/src/libprg/lista_linear.c
+++ libprg/src/libprg/lista_linear.c
@@ -12,6 +12,7 @@ lista_linear_t *criar_lista_linear(int capacidade) {
     lista_linear_t *lista = malloc(sizeof(lista_linear_t));
     lista->elementos = malloc(sizeof(int)*capacidade);
     lista->tamanho = 0;
+    lista->capacidade = capacidade;
 
     return lista;
 }
