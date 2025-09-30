81020502789d53b6cd2af1533133cae27b1a4072
luise <luis.e2003@aluno.ifsc.edu.br>
2025-09-30 09:59:35 -0300

    Implementa função de criar lista encadeada

--- libprg/src/libprg/lista_encadeada.c
+++ libprg/src/libprg/lista_encadeada.c
@@ -1,6 +1,15 @@
+#include <stdlib.h>
 #include "libprg/libprg.h"
 
 typedef struct no {
     int valor;
     struct no *proximo;
 }no_t;
+
+no_t *criar_lista_encadeada(int valor) {
+    no_t *no = malloc(sizeof(no_t));
+    no->valor = valor;
+    no->proximo = NULL;
+
+    return no;
+}
