8fa43f446522d5698a31e2f31c524051675f611c
luis <luiseduardodeabreu@gmail.com>
2025-12-10 16:32:09 -0300

    Altera o método de bubble sort para ordem crescente.

--- libprg/src/libprg/alg_ord_troca.c
+++ libprg/src/libprg/alg_ord_troca.c
@@ -3,7 +3,7 @@
 void bubble_sort(int vetor[], int n) {
 	for (int i=0; i < n; i++) {
 		for (int j=0; j < n-1-i; j++) {
-			if (vetor[j] < vetor[j+1]) {
+			if (vetor[j] > vetor[j+1]) {
 				int aux = vetor[j];
 				vetor[j] = vetor[j+1];
 				vetor[j+1] = aux;
