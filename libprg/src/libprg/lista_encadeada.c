9bd2cbefe02d59e59c038a542b9a3581d1e18a01
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-05-05 17:04:51 -0300

    Move as funções da lista encadeada dupla de lista_encadeada.c para lista_encadeada_dupla.c

--- libprg/src/libprg/lista_encadeada.c
+++ libprg/src/libprg/lista_encadeada.c
@@ -6,12 +6,6 @@ typedef struct no {
     no_t *proximo;
 } no_t;
 
-typedef struct nod {
-    int dado;
-    nod_t *anterior;
-    nod_t *proximo;
-} nod_t;
-
 no_t *criar_lista_encadeada(int valor) {
     no_t *no = malloc(sizeof(no_t));
     no->dado = valor;
@@ -63,63 +57,6 @@ void destruir_encadeada(no_t **inicio) {
 
     while (atual != NULL) {
         no_t *proximo = atual->proximo;
-        free(atual);
-        atual = proximo;
-    }
-}
-
-nod_t *criar_lista_encadeada_dupla(int valor) {
-    nod_t *no = malloc(sizeof(nod_t));
-    no->dado = valor;
-    no->anterior = NULL;
-    no->proximo = NULL;
-
-    return no;
-}
-
-void inserir_encadeada_dupla(nod_t **inicio, int valor) {
-    nod_t *novo_no = criar_lista_encadeada_dupla(valor);
-    novo_no->proximo = *inicio;
-    (*inicio)->anterior = novo_no;
-    *inicio = novo_no;
-}
-
-nod_t *buscar_encadeada_dupla(nod_t **inicio, int valor) {
-    nod_t *atual = *inicio;
-
-    while (atual != NULL) {
-        if (atual->dado == valor)
-            return atual;
-
-        atual = atual->proximo;
-    }
-    return NULL;
-}
-
-int remover_encadeada_dupla(nod_t **inicio, int valor) {
-    nod_t *atual = buscar_encadeada_dupla(inicio, valor);
-
-    if (atual != NULL) {
-        if (atual->anterior != NULL) // Se não é o primeiro nó
-            atual->anterior->proximo = atual->proximo;
-        else
-            *inicio = atual->proximo; // remove o primeiro
-
-        if (atual->proximo != NULL) // Se não é o último nó
-            atual->proximo->anterior = atual->anterior;
-
-        free(atual);
-
-        return 1;
-    }
-    return 0;
-}
-
-void destruir_encadeada_dupla(nod_t **inicio) {
-    nod_t *atual = *inicio;
-
-    while (atual != NULL) {
-        nod_t *proximo = atual->proximo;
         free(atual);
         atual = proximo;
     }
