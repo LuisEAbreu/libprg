d1db0e2c33e81eb960af20f0d20d762b8a08bbc2
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-05-12 16:05:30 -0300

    Implementa método insertion sort

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -64,5 +64,6 @@ void destruir_encadeada_dupla(nod_t **inicio);
 /*===== ALGORITMOS DE ORDENAÇÃO =====*/
 
 void bubble_sort(int vetor[], int tamanho);
+int *insertion_sort(int *vetor, int tamanho);
 
 #endif
--- libprg/src/libprg/ordena.c
+++ libprg/src/libprg/ordena.c
@@ -11,3 +11,15 @@ int *bubble_sort(int *vetor, int tamanho) {
         }
     }
 }
+
+int *insertion_sort(int *vetor, int tamanho) {
+    for (int i = 0; i < tamanho - 1; i++) {
+        int chave = vetor[i];
+        int j = i + 1;
+        while (j >= 0 && vetor[j] > chave) {
+            vetor[j + 1] = vetor[j];
+            j = j - 1;
+        }
+        vetor[j + 1] = chave;
+    }
+}
