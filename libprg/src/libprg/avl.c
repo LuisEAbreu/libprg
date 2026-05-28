f3cc01d2381260030bc936c99f32b1e1ccf5f288
Luis Eduardo <luis.e2003@aluno.ifsc.edu.br>
2026-05-28 16:13:18 -0300

    fix: inverte ordem da soma para remover warning

--- libprg/src/libprg/avl.c
+++ libprg/src/libprg/avl.c
@@ -57,8 +57,8 @@ noavl_t* rotacao_esquerda(noavl_t* raiz) {
     raiz->direita = neto_esquerda;
 
     // atualiza alturas
-    raiz->altura = max(altura_avl(raiz->esquerda), altura_avl(raiz->direita)) + 1;
-    filho_direita->altura = max(altura_avl(filho_direita->esquerda), altura_avl(filho_direita->direita)) + 1;
+    raiz->altura = 1 + max(altura_avl(raiz->esquerda), altura_avl(raiz->direita));
+    filho_direita->altura = 1 + max(altura_avl(filho_direita->esquerda), altura_avl(filho_direita->direita));
 
     // retorna nova raiz
     return filho_direita;
@@ -73,8 +73,8 @@ noavl_t* rotacao_direita(noavl_t* raiz) {
     raiz->esquerda = neto_esquerda;
 
     // atualiza alturas
-    raiz->altura = max(altura_avl(raiz->esquerda), altura_avl(raiz->direita)) + 1;
-    filho_direita->altura = max(altura_avl(filho_direita->esquerda), altura_avl(filho_direita->direita)) + 1;
+    raiz->altura = 1 + max(altura_avl(raiz->esquerda), altura_avl(raiz->direita));
+    filho_direita->altura = 1 + max(altura_avl(filho_direita->esquerda), altura_avl(filho_direita->direita));
 
     // retorna nova raiz
     return filho_direita;
